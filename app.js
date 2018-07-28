13a7ce94ee0ac2a31ccfbc9c7a408da6669af6ed
Shayne Stewart <sh.wc.smack@gmail.com>
2018-07-27 23:18:15 -0500

    Adding user to DB

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-require('./services/passport');
-
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000
+
+mongoose.connect('mongodb://localhost:27017/RevBudget')
+require('./models/user');
+require('./services/passport');
 
 //use authRoutes with created app object
 require('./routes/authRoutes')(app);
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,11 @@
+const mongoose = require('mongoose');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+
+
+const User = mongoose.model('users');
+
 
 passport.use(
   new GoogleStrategy({
@@ -8,8 +13,6 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile)
+    new User({ googleId: profile.id }).save();
   })
 );
