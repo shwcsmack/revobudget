1c36037dfb676b1d3c0bb15db8535441f26c8403
Shayne Stewart <sh.wc.smack@gmail.com>
2018-07-28 09:49:23 -0500

    Finished OAuth basics

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 
 const app = express();
 const PORT = process.env.PORT || 5000
+
+app.use(
+  cookieSession({
+    maxAge: 30*24*60*60*1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/RevBudget')
 require('./models/user');
--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,18 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then((user) => {
+      done(null, user);
+    }).catch((err) => {
+      done(err, null);
+    })
+});
 
 passport.use(
   new GoogleStrategy({
@@ -13,6 +25,19 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save();
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => {
+              done(null, user);
+            });
+        }
+      }).catch((err) => {
+        done(err, null);
+      });
   })
 );
